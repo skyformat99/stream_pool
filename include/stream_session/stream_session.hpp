aa2226ae1fa540e23e9e95b2d7e92604840b536b
Reid Paape <wrpaape@gmail.com>
2017-06-26 20:59:07 -0500

    stream to be just a blocking queue

--- include/stream_session/stream_session.hpp
+++ include/stream_session/stream_session.hpp
@@ -3,9 +3,9 @@
 
 // EXTERNAL DEPENDENCIES
 // =============================================================================
-#include <unordered_map>      // std::unordered_map
+// #include <unordered_map>      // std::unordered_map
 #include <string>             // std::string
-#include <chrono>             // std::chrono::seconds
+#include <chrono>             // std::chrono::*
 #include <condition_variable> // std::condition_variable
 #include <mutex>              // std::mutex
 
@@ -13,14 +13,20 @@
 
 // FORWARD DECLARATIONS
 // =============================================================================
-class StreamPool;
+// class StreamPool;
 
 
 
 class StreamSession
 {
+public:
+    StreamSession()
+    {
+
+    }
+
 private:
-    friend StreamPool;
+    // friend StreamPool;
 
     typedef std::unordered_map<std::string, StreamSession> Register;
     typedef Register::iterator Record;
@@ -31,7 +37,7 @@ private:
     ~StreamSession();
 
 
-    void start();
+    void process(const std::string &payload);
     void stop();
 
     void reader_loop();
@@ -39,8 +45,7 @@ private:
     void watchdog_loop();
 
 
-    static const std::chrono::seconds time_limit(20);
-
+    std::chrono::time_point<std::chrono::system_clock>
     std::condition_variable watchdog_event;
     std::mutex              watchdog_lock;
     Record                  enrollment;
