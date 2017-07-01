37a1e633f7ce1759f501bfb616f657ef11611005
Reid Paape <wrpaape@gmail.com>
2017-06-30 21:31:06 -0500

    string to single char

--- include/stream_session/stream_session.hpp
+++ include/stream_session/stream_session.hpp
@@ -4,10 +4,12 @@
 // EXTERNAL DEPENDENCIES
 // =============================================================================
 // #include <unordered_map>      // std::unordered_map
-#include <string>             // std::string
 #include <chrono>             // std::chrono::*
 #include <condition_variable> // std::condition_variable
 #include <mutex>              // std::mutex
+#include <thread>             // std::thread
+
+#include "stream_pool/stream/stream.hpp" // Stream
 
 
 
@@ -19,25 +21,19 @@
 
 class StreamSession
 {
-public:
-    StreamSession()
-    {
-
-    }
+    friend class StreamPool;
 
 private:
     // friend StreamPool;
 
-    typedef std::unordered_map<std::string, StreamSession> Register;
     typedef Register::iterator Record;
-
 
 
     StreamSession();
     ~StreamSession();
 
 
-    void process(const std::string &payload);
+    void process();
     void stop();
 
     void reader_loop();
@@ -49,6 +45,7 @@ private:
     std::condition_variable watchdog_event;
     std::mutex              watchdog_lock;
     Record                  enrollment;
+    Stream                  stream;
 }; // class StreamSession
 
 #endif // ifndef STREAM_POOL_STREAM_SESSION_STREAM_SESSION_HPP
