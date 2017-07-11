5169af715ab15bc8197282c19a29093816cc109b
Reid Paape <wrpaape@gmail.com>
2017-07-10 21:30:07 -0500

    more on stream_session

--- include/stream/stream.hpp
+++ include/stream/stream.hpp
@@ -1,5 +1,3 @@
-#ifndef STREAM_POOL_STREAM_STREAM_HPP
-#define STREAM_POOL_STREAM_STREAM_HPP
 
 // EXTERNAL DEPENDENCIES
 // =============================================================================
@@ -9,36 +7,32 @@
 #include <atomic>                            // std::atomic
 #include "blocking_queue/blocking_queue.hpp" // BlockingQueue
 #include "failure_box/failure_box.hpp"       // FailureBox
+#include "stream_common/stream_common.hpp"   // FailureBox
 
 
 
 class Stream : private BlockingQueue<std::string>
 {
 public:
+    typedef std::chrono::time_point<std::chrono::system_clock> time_point;
+
     Stream();
     ~Stream();
     bool start();
     bool read(std::string &message);
     bool write(std::string &&payload);
 
-private:
-    typedef std::chrono::time_point<std::chrono::system_clock> time_point;
-    typedef unsigned int                                       outcome_type;
-
-    static const std::chrono::seconds max_lifespan;
-    static const std::chrono::seconds max_downtime;
-    static std::random_device         random_seed;
-    static const outcome_type         outcome_min;
-    static const outcome_type         outcome_max;
-    static const outcome_type         outcome_fail;
-
     static time_point now();
 
-    std::uniform_int_distribution<outcome_type> distribution;
-    std::atomic<bool>                           continue_writing;
-    time_point                                  idle_timeout;
-    const time_point                            expiry;
-    FailureBox                                  failure_box;
+private:
+    static const std::chrono::seconds max_lifespan;
+    static const std::chrono::seconds max_downtime;
+    static const unsigned int         expected_query_lifespan;
+
+    std::atomic<bool> continue_writing;
+    time_point        idle_timeout;
+    const time_point  expiry;
+    FailureBox        failure_box;
 }; // class Stream
 
 #endif // ifndef STREAM_POOL_STREAM_STREAM_HPP
--- include/stream_session/stream_session.hpp
+++ include/stream_session/stream_session.hpp
@@ -4,13 +4,13 @@
 // EXTERNAL DEPENDENCIES
 // =============================================================================
 // #include <unordered_map>      // std::unordered_map
-#include <chrono>             // std::chrono::*
+#include <ostream>            // std::ostream
 #include <condition_variable> // std::condition_variable
 #include <mutex>              // std::mutex
 #include <thread>             // std::thread
 
 #include "blocking_queue/blocking_queue.hpp" // BlockingQueue
-#include "stream/stream.hpp"                 // Stream
+#include "stream/stream.hpp"                 // Stream::
 #include "stream_pool/stream_pool.hpp"       // StreamPool
 
 #ifdef STREAM_POOL_TESTING_ENABLED
@@ -28,18 +28,24 @@
 class StreamSession
 {
 public:
-    StreamSession(StreamPool *const pool);
+    StreamSession(const char id,
+                  StreamPool *const pool);
     ~StreamSession();
 
 
     void process();
 
 private:
+    friend std::thread;
+
     void reader_loop();
     void writer_loop();
     void watchdog_loop();
 
-    std::chrono::time_point<std::chrono::system_clock>
+    std::ostream &log(const char *const function_name);
+
+    const char              id;
+    Stream::time_point      timeout;
     std::condition_variable watchdog_event;
     std::mutex              watchdog_lock;
     std::atomic<bool>       keep_alive;
