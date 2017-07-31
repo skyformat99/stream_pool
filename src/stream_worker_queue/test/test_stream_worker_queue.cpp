3c6e07cd2a74db289156cd369572fb55bfdaaaab
Reid Paape <wrpaape@gmail.com>
2017-07-30 22:20:42 -0500

    more boilerplate

---
+++ src/stream_worker_queue/test/test_stream_worker_queue.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"                               // TEST, assert macros
+#include "stream_worker_queue/stream_worker_queue.hpp" // StreamWorkerQueue
+
+TEST(constructor, a)
+{
+}
+
+
+TEST(constructor, b)
+{
+}
